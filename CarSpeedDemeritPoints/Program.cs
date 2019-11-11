30e6bee94295c2a12e8e213f11c6489c5c376819
Sachin Gurung <saching061@hotmail.com>
2019-11-10 20:43:33 -0800

    First Commit

--- CarSpeedDemeritPoints/Program.cs
+++ CarSpeedDemeritPoints/Program.cs
@@ -10,6 +10,43 @@ namespace CarSpeedDemeritPoints
     {
         static void Main(string[] args)
         {
+            int speedLimit;
+            int carSpeed;
+            int Points = 0;
+            int counter;
+
+            Console.Write("Enter the speed limit in km/hr: ");
+            speedLimit = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the car speed in km/hr: ");
+            carSpeed = int.Parse(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                int speedDifference = carSpeed - speedLimit;
+                for (counter = 5; counter <= speedDifference; counter++)
+                {
+                    if (counter % 5 == 0)
+                    {
+                        Points++;
+                    }
+                }
+            }
+            Console.Write("Demerit points: ");
+            Console.WriteLine(Points);
+
+            if (Points > 12)
+                Console.WriteLine("Your Lincense is suspended");
         }
     }
 }
+/*
+Enter the speed limit in km/hr: 70
+Enter the car speed in km/hr: 85
+Demerit points: 3
+Press any key to continue . . .
+*/
